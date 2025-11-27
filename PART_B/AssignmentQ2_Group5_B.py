d3b0375dc66efeddc2c4cce31ed74048fcbdacff
StefanKlaassen <stefan.klaassen@outlook.com>
2025-11-27 16:32:48 +0100

    Model Bart gemaakt

--- PART_B/AssignmentQ2_Group5_B.py
+++ PART_B/AssignmentQ2_Group5_B.py
@@ -8,7 +8,7 @@ Authors:
     Peter Nederveen - 
     Britt van de Geer - 
     Bart Verzijl - 
-Last updated: 2025-11-24
+Last updated: 2025-11-27
 Version: 1.0
 
 Usage:
@@ -27,7 +27,7 @@ Dependencies:
 
 MAX_FLOAT = 1e6
 VEHICLES = 4
-VEHICLE_VELOCITY = 2
+VEHICLE_PACE = 2
 VEHICLE_CAPACITY = 120
 VEHICLE_RANGE = 110
 VEHICLE_CHARGE_RATE = 1.0
@@ -91,17 +91,17 @@ N = range(len(node_data))
 V = range(VEHICLES)
 
 # PARAMETERS
-bm = VEHICLE_CAPACITY                       # Maximimum battery capcity
+c  = [VEHICLE_CAPACITY for _ in V]          # Vehicle capacity per vehicle v
+d  = build_distance_mat(node_data)          # Distance between node i and j
+s  = [VEHICLE_PACE for _ in V]              # Velocity of vehicle v
+q  = [n.DEMAND for n in node_data]          # Demand at node i
+bm = [VEHICLE_RANGE for _ in V]             # Maximum battery capacity of vehicle v
 bc = VEHICLE_CHARGE_RATE                    # Energy charging rate
-bd = VEHICLE_DISCHARGE_RATE                 # Energy discharging rate [battery_unit/time_unit]
-bs = [n.CHARGING for n in node_data]        # Charger station at node j [battery_unit]
-c  = VEHICLE_CAPACITY                       # Vehicle capacity [demand_unit]
-d  = build_distance_mat(node_data)          # Distance between node i and j [distance_unit]
-s  = VEHICLE_VELOCITY                       # Velocity [time_unit/distance_unit]
-q  = [n.DEMAND for n in node_data]          # Demand at node i [demand_unit]
-ts = [n.SERVICETIME for n in node_data]     # Minimal service time at node j [time_unit]
-tr = [n.READYTIME for n in node_data]       # Ready time [time_unit]
-td = [n.DUETIME for n in node_data]         # Due time [time_unit]
+bd = VEHICLE_DISCHARGE_RATE                 # Energy discharging rate
+bs = [n.CHARGING for n in node_data]        # Charger station at node i
+ts = [n.SERVICETIME for n in node_data]     # Minimal service time at node i
+tr = [n.READYTIME for n in node_data]       # Ready time at node i
+td = [n.DUETIME for n in node_data]         # Due time at node i
 
 
 # MODEL DEFINITION
@@ -111,26 +111,54 @@ td = [n.DUETIME for n in node_data]         # Due time [time_unit]
 model = Model('Vehicle Routing Problem')
 
 # DESISION VARIABLES
-x = model.addVars(N, N, V, vtype=GRB.BINARY, name='x')
-z = model.addVars(N, V, vtype=GRB.BINARY, name='z')
+xt = model.addVars(N, N, V, vtype=GRB.BINARY, name='x')     # If 1, indicates if vehicle v travels from node i to j
+xv = model.addVars(N, V, vtype=GRB.BINARY, name='z')        # If 1, node is visited by vehicle v 
+xa = model.addVars(N, lb=0, name='τ^a')                     # Time of arrival at node i
+xc = model.addVars(N, lb=0, name='τ^c')                     # Time at node i with charging
+xw = model.addVars(N, lb=0, name='τ^w')                     # Time at node i without charging
+xb = model.addVars(N, V, lb=0, name='β')                    # Battery level of vehicle v at node i
 
 # OBJECTIVE
-obj = quicksum(d[i][j] * x[i, j, v] for i in N for j in N for v in V)
+obj = quicksum(d[i][j] * xt[i, j, v] for i in N for j in N for v in V)
 model.setObjective(obj, GRB.MINIMIZE)
 
 # CONSTRAINTS
 constraints = {
     'visit_constraint':
-    (quicksum(z[i, v] for v in V) == 1 for i in N),
+    (quicksum(xv[i, v] for v in V) == 1 for i in N[1:]),
 
     'depot_constraint':
-    quicksum(z[0, v] for v in V) == VEHICLES,
+    quicksum(xv[0, v] for v in V) == VEHICLES,
 
-    'vehicle_capacity_constraint':
-    (quicksum(bm * z[i, v] for i in N) for v in V),
+    'vehicle_capacity_constraint': #TODO
+    (quicksum(c[i] * xv[i, v] for i in N) for v in V),
 
     'departure_constraint':  
-    (quicksum(x[i, j, v] for j in N) == quicksum(x[j, i, v] for j in N) == z[i, v] for i in N for v in V)
+    (quicksum(xt[i, j, v] for j in N) == quicksum(xt[j, i, v] for j in N) == xv[i, v] for i in N for v in V),
+
+    'time_constraint':
+    ...,
+
+    'time_window_constraint':
+    (tr[i] <= xa[i] <= td[i] for i in N),
+
+    'service_time_constraint':
+    (xc[i] + xw[i] >= ts[i] for i in N),
+
+    'battery_capacity_constraint':
+    (xb[i, v] - xt[i, j, v] * (d[i][j] * s[v] * bd) >= 0 
+        for i in N for j in N for v in V),
+
+    'battery_capacity_constraint(2)':
+    (xb[i, v] - xt[i, j, v] * (d[i][j] * s[v] * bd) + xc[j] * bc >= bm[v]
+        for i in N for j in N[1:] for v in V),
+
+    'battery_update_constraint':
+    (xb[i, v] - xt[i, j, v] * (d[i][j] * s[v] * bd) + xc[j] * bc == xb[j, v]
+        for i in N for j in N[1:] for v in V),
+
+    'initial_battery_constraint':
+    (xb[0, v] == bm[v] for v in V)
 }
 
 for name, con in constraints.items():
@@ -145,3 +173,6 @@ model.write('TSPmodel.lp')
 setattr(model.Params, 'timeLimit', 3600)
 model.optimize()
 model.write('TSPmodel.sol')
+
+z = model.ObjVal if model.Status == GRB.OPTIMAL else "😢"
+print('\n'*2, 'Result: ', z)
